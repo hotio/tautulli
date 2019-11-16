305bc4299333764eb84c89f22fe3a867eec19016
hotio <hotio@protonmail.com>
2019-11-16 09:49:42 +0100

    [CI SKIP] add failsafe

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 version=$(curl -fsSL "https://api.github.com/repos/Tautulli/Tautulli/releases" | jq -r .[0].tag_name | sed s/v//g)
+[[ -z ${version} ]] && exit
 find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG TAUTULLI_VERSION=.*$/ARG TAUTULLI_VERSION=${version}/g" {} \;
 sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
 echo "##[set-output name=version;]${version}"
