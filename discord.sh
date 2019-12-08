423751cc8475171bf21dc9f5a09778901ea6dc0b
hotio <hotio@protonmail.com>
2019-12-08 13:08:01 +0100

    add thumb

--- discord.sh
+++ discord.sh
@@ -4,11 +4,11 @@ if [[ "${DRONE_BUILD_STATUS}" == "success" ]]; then
     message="succeeded"
     color="65280"
     [[ -f "/drone/src/screenshot.log" ]] && screenshot_url=$(cat "/drone/src/screenshot.log")
-    [[ -n "${screenshot_url}" ]] && screenshot_message="\n[screenshot](${screenshot_url})"
+    [[ -n "${screenshot_url}" ]] && thumbnail=',"thumbnail": {"url": "'${screenshot_url}'"}'
 else
     message="failed"
     color="16711680"
 fi
 
 curl -fsSL -H "Content-Type: application/json" \
--X POST -d '{"embeds":[{"description":"Build **[#'"${DRONE_BUILD_NUMBER}"'](https://cloud.drone.io/'"${DRONE_REPO}"'/'"${DRONE_BUILD_NUMBER}"')** of `'"${DRONE_REPO//docker-/}"':'"${DRONE_COMMIT_BRANCH}"'` '"${message}"'.'"${screenshot_message}"'\n\nCommit **['"${DRONE_COMMIT_SHA:0:7}"']('"${DRONE_COMMIT_LINK}"')** by **'"${DRONE_COMMIT_AUTHOR}"'**:\n```'"${DRONE_COMMIT_MESSAGE//\"/}"'```","color":'"${color}"',"fields":[{"name":"Version","value":"'"${VERSION}"'"}]}]}' "https://discordapp.com/api/webhooks/${DISCORD_ID}/${DISCORD_TOKEN}"
+-X POST -d '{"embeds":[{"description":"Build **[#'"${DRONE_BUILD_NUMBER}"'](https://cloud.drone.io/'"${DRONE_REPO}"'/'"${DRONE_BUILD_NUMBER}"')** of `'"${DRONE_REPO//docker-/}"':'"${DRONE_COMMIT_BRANCH}"'` '"${message}"'.\n\nCommit **['"${DRONE_COMMIT_SHA:0:7}"']('"${DRONE_COMMIT_LINK}"')** by **'"${DRONE_COMMIT_AUTHOR}"'**:\n```'"${DRONE_COMMIT_MESSAGE//\"/}"'```","color":'"${color}"',"fields":[{"name":"Version","value":"'"${VERSION}"'"}]'"${thumbnail}"'}]}' "https://discordapp.com/api/webhooks/${DISCORD_ID}/${DISCORD_TOKEN}"
