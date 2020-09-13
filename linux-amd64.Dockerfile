a26d71c63e81dc2128e0c20867d7784813538f6a
hotio <26902309+hotio@users.noreply.github.com>
2020-09-13 20:38:59 +0200

    rework build

--- linux-amd64.Dockerfile
+++ linux-amd64.Dockerfile
@@ -7,26 +7,11 @@ RUN apk add --no-cache python3 py3-lxml py3-openssl py3-setuptools && \
     pip3 install --no-cache-dir --upgrade plexapi pycryptodomex && \
     apk del --purge build-dependencies
 
-ARG TAUTULLI_VERSION
-ARG TAUTULLI_BRANCH
-RUN curl -fsSL "https://github.com/Tautulli/Tautulli/archive/v${TAUTULLI_VERSION}.tar.gz" | tar xzf - -C "${APP_DIR}" --strip-components=1 && \
+ARG VERSION
+ARG BRANCH
+RUN curl -fsSL "https://github.com/Tautulli/Tautulli/archive/v${VERSION}.tar.gz" | tar xzf - -C "${APP_DIR}" --strip-components=1 && \
     chmod -R u=rwX,go=rX "${APP_DIR}" && \
-    echo "v${TAUTULLI_VERSION}" > "${APP_DIR}/version.txt" && \
-    echo "${TAUTULLI_BRANCH}" > "${APP_DIR}/branch.txt"
+    echo "v${VERSION}" > "${APP_DIR}/version.txt" && \
+    echo "${BRANCH}" > "${APP_DIR}/branch.txt"
 
 COPY root/ /
-
-ARG LABEL_CREATED
-LABEL org.opencontainers.image.created=$LABEL_CREATED
-ARG LABEL_TITLE
-LABEL org.opencontainers.image.title=$LABEL_TITLE
-ARG LABEL_REVISION
-LABEL org.opencontainers.image.revision=$LABEL_REVISION
-ARG LABEL_SOURCE
-LABEL org.opencontainers.image.source=$LABEL_SOURCE
-ARG LABEL_VENDOR
-LABEL org.opencontainers.image.vendor=$LABEL_VENDOR
-ARG LABEL_URL
-LABEL org.opencontainers.image.url=$LABEL_URL
-ARG LABEL_VERSION
-LABEL org.opencontainers.image.version=$LABEL_VERSION
--- update.sh
+++ update.sh
@@ -9,16 +9,34 @@ if [[ ${1} == "checkdigests" ]]; then
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile  && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm-v7.Dockerfile && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile  && echo "${digest}"
+elif [[ ${1} == "tests" ]]; then
+    echo "List installed packages..."
+    docker run --rm --entrypoint="" "${2}" apk -vv info | sort
+    echo "Check if app works..."
+    app_url="http://localhost:8181"
+    docker run --rm --network host -d --name service -e DEBUG="yes" "${2}"
+    currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${app_url}" > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
+    curl -fsSL "${app_url}" > /dev/null
+    status=$?
+    echo "Show docker logs..."
+    docker logs service
+    exit ${status}
+elif [[ ${1} == "screenshot" ]]; then
+    app_url="http://localhost:8181"
+    docker run --rm --network host -d --name service -e DEBUG="yes" "${2}"
+    currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${app_url}" > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
+    docker run --rm --network host --entrypoint="" -u "$(id -u "$USER")" -v "${GITHUB_WORKSPACE}":/usr/src/app/src zenika/alpine-chrome:with-puppeteer node src/puppeteer.js
+    exit 0
 else
     data=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/Tautulli/Tautulli/releases")
     version=$(jq -r .[0].tag_name <<< "${data}" | sed s/v//g)
     [[ -z ${version} ]] && exit 1
-    sed -i "s/{TAUTULLI_VERSION=[^}]*}/{TAUTULLI_VERSION=${version}}/g" .github/workflows/build.yml
+    echo "VERSION=${version}" > VERSION
     prerelease=$(jq -r .[0].prerelease <<< "${data}")
     if [[ ${prerelease} == true ]]; then
-        sed -i "s/{TAUTULLI_BRANCH=[^}]*}/{TAUTULLI_BRANCH=beta}/g" .github/workflows/build.yml
+        echo "BRANCH=beta" >> VERSION
     else
-        sed -i "s/{TAUTULLI_BRANCH=[^}]*}/{TAUTULLI_BRANCH=master}/g" .github/workflows/build.yml
+        echo "BRANCH=master" >> VERSION
     fi
     echo "##[set-output name=version;]${version}"
 fi
