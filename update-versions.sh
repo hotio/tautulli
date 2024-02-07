74d0a772132ce9bcbe345e6a91b1732b88dffc65
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 10:24:42 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-
-data=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/Tautulli/Tautulli/releases")
-version=$(jq -r .[0].tag_name <<< "${data}" | sed s/v//g)
-[[ -z ${version} ]] && exit 0
-prerelease=$(jq -r .[0].prerelease <<< "${data}")
-if [[ ${prerelease} == true ]]; then
-    branch=beta
-else
-    branch=master
-fi
-old_version=$(jq -r '.version' < VERSION.json)
-changelog=$(jq -r '.changelog' < VERSION.json)
-[[ "${old_version}" != "${version}" ]] && changelog="https://github.com/tautulli/tautulli/compare/v${old_version}...v${version}"
-version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'" | .git_branch = "'"${branch}"'" | .changelog = "'"${changelog}"'"' <<< "${version_json}" > VERSION.json
+set -e
+git_branch="master"
+response_json=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/Tautulli/Tautulli/releases")
+version=$(jq -re '.[0].tag_name' <<< "${response_json}")
+prerelease=$(jq -r '.[0].prerelease' <<< "${response_json}")
+[[ ${prerelease} == true ]] && git_branch="beta"
+json=$(cat VERSION.json)
+jq --sort-keys \
+    --arg version "${version//v/}" \
+    --arg git_branch "${git_branch}" \
+    '.version = $version | .git_branch = $git_branch' <<< "${json}" | tee VERSION.json
