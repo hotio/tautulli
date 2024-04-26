c188c039715388edd102ee5a61ee1844d5366b4d
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-04-26 06:52:32 +0200

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -2,6 +2,8 @@
 git_branch="master"
 response_json=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/Tautulli/Tautulli/releases/latest") || exit 1
 version=$(jq -re '.tag_name' <<< "${response_json}")
+[[ -z ${version} ]] && exit 0
+[[ ${version} == null ]] && exit 0
 prerelease=$(jq -r '.prerelease' <<< "${response_json}")
 [[ ${prerelease} == true ]] && git_branch="beta"
 json=$(cat VERSION.json)
