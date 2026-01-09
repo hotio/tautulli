d0921bdf1130bc9be53e7e6ebdf49d136696f4d6
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-09 09:28:17 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+set -exuo pipefail
+
 git_branch="master"
-response_json=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/Tautulli/Tautulli/releases") || exit 1
+response_json=$(curl -fsSL "https://api.github.com/repos/Tautulli/Tautulli/releases")
 version=$(jq -re '.[0].tag_name' <<< "${response_json}")
-[[ -z ${version} ]] && exit 0
-[[ ${version} == null ]] && exit 0
 prerelease=$(jq -r '.[0].prerelease' <<< "${response_json}")
 [[ ${prerelease} == true ]] && git_branch="beta"
 json=$(cat VERSION.json)
